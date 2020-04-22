ea8fada8d54960792464db1d91d3b48ccda576ec
Sandra <sandra.bj0308@gmail.com>
2020-04-22 14:40:42 +0200

    Añade getters y setters en clase modelo

--- src/main/java/daw/sandrabazan/Persona.java
+++ src/main/java/daw/sandrabazan/Persona.java
@@ -26,6 +26,33 @@ public class Persona {
         this.apellidos = "Navarro";
         this.nif = "11223344N";
     }
+
+    // Métodos getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+    
+    
     
     
 }
